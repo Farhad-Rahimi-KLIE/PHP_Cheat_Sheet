560c54f904afc687826a749ce666b64901aebc5b
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-20 14:49:03 +0430

    Update Loops.php

--- Loops.php
+++ Loops.php
@@ -1,2 +1,2 @@
-Iterative statements or Loops facilitate programmers to execute any block of code lines
+Iterative Statements or Loops facilitate programmers to execute any block of code lines
 repeatedly.
