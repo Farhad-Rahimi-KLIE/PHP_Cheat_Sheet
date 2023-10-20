519699dffb6a5265c1efd41b9602310452e1c160
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-20 14:32:38 +0430

    Update $_GET.php

--- $_GET.php
+++ $_GET.php
@@ -1,4 +1,4 @@
-PHP $_GET is a PHP super global variable which is used to collect form data after submitting an
+PHP $_GET is a PHP Super global variable which is used to collect form data after submitting an
 HTML form with method="get".
 <?php
 echo "Hello" . $_GET['Example'] . " at " . $_GET['web'];
