fc8ab34c4d078e9027a8fccdad8d7f19c1847505
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-20 05:14:32 +0430

    Add files via upload

---
+++ get_resource_type.php
@@ -0,0 +1,9 @@
+It returns the resource type:
+<?php
+// prints: stream 
+$fp = fopen("foo", "w");
+echo get_resource_type($fp) . "\n";
+// prints: curl
+$c = curl_init ();
+echo get_resource_type($c) . "\n"; // works prior to PHP 8.0.0 as since 8.0 
+?>
