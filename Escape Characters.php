8ab89334d7fd3839d078107e2acf9357de19ca0a
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-20 14:44:11 +0430

    Update Escape Characters.php

--- Escape Characters.php
+++ Escape Characters.php
@@ -1,2 +1,2 @@
-Escape sequences are used for escaping a character during string parsing. It is also used for
+Escape Sequences are used for escaping a character during string parsing. It is also used for
 giving special meaning to represent line breaks, tabs, alerts and more.
