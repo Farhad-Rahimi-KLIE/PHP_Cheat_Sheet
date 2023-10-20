ebc3108849d7c3576716f2599e2b227ceb721620
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-20 05:23:34 +0430

    Add files via upload

---
+++ PHP Logical Operators.php
@@ -0,0 +1,18 @@
+And
+True if both $x and $y are true
+$x and $y
+Or
+True if either $x or $y is true
+$x or $y
+Xor
+True if either $x or $y is true, but not both
+$x xor $y
+And
+True if both $x and $y are true
+$x && $y
+Or
+True if either $x or $y is true
+$x || $y
+Not
+True if $x is not true
+!$x
