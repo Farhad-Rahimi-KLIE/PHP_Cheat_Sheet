a9989d95c39a1aba4fb3a715e28b1eb351fa76c1
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-20 15:43:24 +0430

    Update Foreach Loop.php

--- Foreach Loop.php
+++ Foreach Loop.php
@@ -1,4 +1,4 @@
-The Foreach loop loops through a block of code for each element in an array.
+The Foreach Loop Loops through a block of code for each element in an array.
 foreach ($InsertYourArrayName as $value) {
 // code to execute goes here 
 }
