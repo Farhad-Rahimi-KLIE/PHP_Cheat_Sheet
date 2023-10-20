d435f97da33f4705584223890c963c57c264361b
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-20 15:54:45 +0430

    Update While Loop.php

--- While Loop.php
+++ While Loop.php
@@ -1,4 +1,4 @@
-It Iterate the block of code as long as a specified condition is True or vice versa
+It Iterate the Block of code as long as a specified condition is True or vice versa
 while (condition that must apply) {
 // code to execute goes here 
 }
