9c0949ebea6406415c30d10d403ce42df9d4914a
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-20 05:05:04 +0430

    Add files via upload

---
+++ $_REQUEST.php
@@ -0,0 +1,20 @@
+PHP $_REQUEST is a PHP super global variable which is used to collect data after submitting an
+HTML form.
+<html>
+<body>
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+Name: <input type="text" name="fname">
+<input type="submit">
+</form>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+$name = $_REQUEST['fname'];
+if (empty($name)) {
+echo "Name is empty";
+} else {
+echo $name;
+}
+}
+?>
+</body>
+</html>
