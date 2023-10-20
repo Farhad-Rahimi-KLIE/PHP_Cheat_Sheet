e7bba2e16978fdac33d556b338848d2c66a880c3
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-20 15:53:42 +0430

    Update Variable-handling Functions.php

--- Variable-handling Functions.php
+++ Variable-handling Functions.php
@@ -1,2 +1,2 @@
-The PHP Variable handling functions are part of the PHP core. No installation is required to use
+The PHP Variable handling Functions are part of the PHP core. No installation is required to use
 these functions.
