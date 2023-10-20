3b22d511d9c6bd57762ba828b616c078cfb2b6b5
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-20 15:50:24 +0430

    Update PHP Conditional Assignment Operators.php

--- PHP Conditional Assignment Operators.php
+++ PHP Conditional Assignment Operators.php
@@ -1,4 +1,4 @@
 Ternary
-Returns The value of $x. The value of $x is expr2 if expr1 = TRUE. The value of $x is expr3 if expr1
+Returns The Value of $x. The value of $x is expr2 if expr1 = TRUE. The value of $x is expr3 if expr1
 = FALSE
 $x = expr1 ? expr2 : expr3
