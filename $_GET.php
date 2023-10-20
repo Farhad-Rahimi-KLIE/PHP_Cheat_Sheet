c15280d3c12e79e53a99abf14aa47f918f2b7c29
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-20 05:04:35 +0430

    Add files via upload

---
+++ $_GET.php
@@ -0,0 +1,5 @@
+PHP $_GET is a PHP super global variable which is used to collect form data after submitting an
+HTML form with method="get".
+<?php
+echo "Hello" . $_GET['Example'] . " at " . $_GET['web'];
+?>
