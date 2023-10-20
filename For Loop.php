7186f9dadcffe234ceb79c33d72c0e1829ec0379
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-20 05:12:37 +0430

    Add files via upload

---
+++ For Loop.php
@@ -0,0 +1,6 @@
+It is used to iterate the statements several times. It is frequently used to traverse the data
+structures like the array and linked list.
+for (starting counter value; ending counter value; increment by which 
+to increase) {
+// code to execute goes here 
+}
