80719c3aceb609adc4bdf483895c5fa3ca9632a3
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-20 05:23:15 +0430

    Add files via upload

---
+++ PHP Comparison Operators.php
@@ -0,0 +1,25 @@
+Equal
+Returns true if $x is equal to $y
+$x == $y
+Identical
+Returns true if $x is equal to $y, and they are of the same type
+$x === $y
+Not equal
+Returns true if $x is not equal to $y
+$x != $y
+Not equal
+Returns true if $x is not equal to $y
+$x <> $y
+Not identical
+Returns true if $x is not equal to $y, or they are not of the same type
+$x !== $y
+Greater than
+Returns true if $x is greater than $y
+$x > $y
+Less than
+Returns true if $x is less than $y
+$x < $y
+Greater than or equal to
+Returns true if $x is greater than or equal to $y
+$x >= $y
+Less than or equal to
