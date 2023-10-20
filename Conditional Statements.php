c57ceb741a402215aab22414bc2c938d8d536138
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-20 05:08:25 +0430

    Add files via upload

---
+++ Conditional Statements.php
@@ -0,0 +1,20 @@
+Conditional statements are used to perform operations based on some condition.
+If Statement
+if statement checks the condition and if it is True, then the block of if statement executes;
+otherwise, control skips that block of code.
+if (condition) {
+// code to execute if condition is met 
+}
+If..Else
+if the condition of if block evaluates to True, then if block executes otherwise else block executes
+if (condition) {
+// code to execute if condition is met 
+} else {
+// code to execute if condition is not met 
+}
+If..Elseif..Else
+It executes different codes for more than two conditions
+if (condition) {
+// code to execute if condition is met 
+} elseif (condition) {
+// code to execute if this condition is met
