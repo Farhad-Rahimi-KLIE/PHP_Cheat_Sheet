7b9eb159efea969db8dcf8d3b3e16066c88e74ee
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-20 05:24:10 +0430

    Add files via upload

---
+++ Predefined Variables.php
@@ -0,0 +1,3 @@
+PHP provides a large number of predefined variables to all scripts. The variables represent
+everything from external variables to built-in environment variables, last error messages etc. All
+this information is defined in some predefined variables.
