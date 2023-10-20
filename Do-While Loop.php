e567560cc564b9a70a10ee5f4c51eb52d53de495
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-20 05:09:43 +0430

    Add files via upload

---
+++ Do-While Loop.php
@@ -0,0 +1,5 @@
+This loop is very similar to the while loop with one difference, i.e., the body of the do-while loop
+is executed at least once even if the condition is False. It is an exit-controlled loop.
+do {
+// code to execute goes here; 
+} while (condition that must apply);
