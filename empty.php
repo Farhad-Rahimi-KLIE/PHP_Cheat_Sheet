e0f5a2744f778e3d1f82ae29c3edf58724352ab9
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-20 05:11:07 +0430

    Add files via upload

---
+++ empty.php
@@ -0,0 +1,12 @@
+Empty is used to check whether a variable is empty or not.
+<?php
+$var = 0;
+// Evaluates to true because $var is empty 
+if (empty($var)) {
+echo '$var is either 0, empty, or not set at all';
+}
+// Evaluates as true because $var is set 
+if (isset($var)) {
+echo '$var is set even though it is empty';
+}
+?>
