acd63555b1d033ed1ec47e29a887c8fce37dbec8
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-20 15:42:50 +0430

    Update Float.php

--- Float.php
+++ Float.php
@@ -1,4 +1,4 @@
-A Float is a number with a decimal point or a number in exponential form.
+A Float is a Number with a decimal point or a number in exponential form.
 <?php
 $x = 1.2345;
 var_dump($x);
