e1d698c7b7e26a003b97aced1d594f45ac8eef11
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-20 05:22:38 +0430

    Add files via upload

---
+++ PHP Array Operators.php
@@ -0,0 +1,18 @@
+Union
+Union of $x and $y
+$x + $y
+Equality
+Returns true if $x and $y have the same key/value pairs
+$x == $y
+Identity
+Returns true if $x and $y have the same key/value pairs in the same order and of the same types
+$x === $y
+Inequality
+Returns true if $x is not equal to $y
+$x != $y
+Inequality
+Returns true if $x is not equal to $y
+$x <> $y
+Non-identity
+Returns true if $x is not identical to $y
+$x !== $y
