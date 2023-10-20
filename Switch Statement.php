0ac3937bbbd0e4301348822265e27fee10aa2188
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-20 05:25:29 +0430

    Add files via upload

---
+++ Switch Statement.php
@@ -0,0 +1,15 @@
+It allows a variable to be tested for equality against a list of values (cases).
+switch (n) {
+case x:
+code to execute if n=x;
+break;
+case y:
+code to execute if n=y;
+break;
+case z:
+code to execute if n=z;
+break;
+// add more cases as needed 
+default:
+code to execute if n is neither of the above;
+}
