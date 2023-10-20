96b06b9ef05ba784873667c43249749ac67caa7a
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-20 15:39:53 +0430

    Update Do-While Loop.php

--- Do-While Loop.php
+++ Do-While Loop.php
@@ -1,4 +1,4 @@
-This loop is very Similar to the while loop with one difference, i.e., the body of the do-while loop
+This loop is Very Similar to the while loop with one difference, i.e., the body of the do-while loop
 is executed at least once even if the condition is False. It is an exit-controlled loop.
 do {
 // code to execute goes here; 
