b4a836a67958ab2232fe2954de981d95f2f07ac8
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-20 05:22:22 +0430

    Add files via upload

---
+++ PHP Assignment Operators.php
@@ -0,0 +1,21 @@
+The PHP assignment operators are used with numeric values to write a value to a variable.
+
+x = y
+The left operand gets set to the value of the expression on the right
+
+x = y
+x += y
+Addition
+x = x + y
+x -= y
+Subtraction
+x = x - y
+x *= y
+Multiplication
+x = x * y
+x /= y
+Division
+x = x / y
+x %= y
+Modulus
+x = x % y
