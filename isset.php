57dedcbff603c7a9b7df0a15acb289e2d40274ca
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-20 05:16:03 +0430

    Add files via upload

---
+++ isset.php
@@ -0,0 +1,7 @@
+It is used to check whether a variable is empty. It also checks whether the variable is set/declared:
+<?php
+$x = 0;
+// True because $x is set 
+if (isset($x)) {
+echo "Variable 'x' is set";
+}
