510b259308d4bf7787bcb0d74cfa7a6f4544cb96
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-20 15:37:53 +0430

    Update Comments.php

--- Comments.php
+++ Comments.php
@@ -1,2 +1,2 @@
-Commets are used to Make the code more understandable for programmer, they are not
+Commets are used to Make the Code more understandable for programmer, they are not
 executed by compiler or interpreter.
