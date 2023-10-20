b2628503bae67441ca47e7bcde55b6b508efe0f2
FarhadKlieCoding <farhadrahimiklie786@gmail.com>
2023-10-20 05:23:51 +0430

    Add files via upload

---
+++ PHP Increment  Decrement Operators.php
@@ -0,0 +1,12 @@
+Pre-increment
+Increments $x by one, then returns $x
+=++$x
+Post-increment
+Returns $x, then increments $x by one
+$x++
+Pre-decrement
+Decrements $x by one, then returns $x
+--$x
+Post-decrement
+Returns $x, then decrements $x by one
+$x--
