ecda05b80f23be8ee87c50334137ab340adad7d6
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-24 05:24:21 +0430

    Update $GLOBALS.php

--- $GLOBALS.php
+++ $GLOBALS.php
@@ -1,4 +1,4 @@
-$GLOBALS is a PHP Super Global variable which is used to access global variables from anywhere
+$GLOBALS is a Php Super Global variable which is used to access global variables from anywhere
 in the PHP script.
 <?php
 $a = 10;
