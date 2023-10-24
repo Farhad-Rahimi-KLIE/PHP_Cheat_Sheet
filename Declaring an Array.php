610274f1ddc08d51c6d60c6e3f63b938703edafb
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-25 03:40:16 +0430

    Update Declaring an Array.php

--- Declaring an Array.php
+++ Declaring an Array.php
@@ -1,5 +1,5 @@
 <?php
-$cms = array("Farhad", "Lovish", "Rohan");
+$cms = array("Farhad", "Shaher", "Rohan");
 echo "Who Needs chocolate? Is it" . $cms[0] . ", " .
 $cms[1] . " or " . $cms[2] . "?";
 ?>
