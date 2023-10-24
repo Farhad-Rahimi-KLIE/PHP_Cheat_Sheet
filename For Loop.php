cc7a5e25b1dd334f79e4f712d713290fb32c85d3
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-25 03:43:18 +0430

    Update For Loop.php

--- For Loop.php
+++ For Loop.php
@@ -2,5 +2,5 @@ It is used to Iterate the Statements several times. It is frequently used to tra
 structures like the array and linked list.
 for (starting counter value; ending counter value; increment by which 
 to increase) {
-// code to execute goes here 
+// code to execute goes here
 }
