ba75ce69dc3a4a1abf8f5c6a10f2c54657be2109
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-24 05:25:30 +0430

    Update $_REQUEST.php

--- $_REQUEST.php
+++ $_REQUEST.php
@@ -1,4 +1,4 @@
-PHP $_REQUEST is a PHP Super Global variable which is used to collect data after submitting an
+PHP $_REQUEST is a Php Super Global variable which is used to collect data after submitting an
 HTML form.
 <html>
 <body>
