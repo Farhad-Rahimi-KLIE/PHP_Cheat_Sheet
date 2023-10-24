9ee72be71397307f42e8b48d7112bf4272d50c0a
Farhad Rahimi KLIE <farhadrahimiklie786@gmail.com>
2023-10-25 03:34:47 +0430

    Update $_POST.php

--- $_POST.php
+++ $_POST.php
@@ -1,5 +1,5 @@
 PHP $_POST is a Php Super Global Variable which is used to collect form data after submitting an
-HTML form with method="post". $_POST is also widely used to pass variables.
+HTML form with Method="post". $_POST is also widely used to pass variables.
 <html>
 <body>
 <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
